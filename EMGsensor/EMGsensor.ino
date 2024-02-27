2b9e35ad567a8abc0ba397876f36d1a1663fbce4
bakhwee-bug <parksy8103@gmail.com>
2024-02-27 13:15:11 +0900

    [c4d]성아 꽃 애니메이션+vfx

---
+++ EMGsensor/EMGsensor.ino
@@ -0,0 +1,23 @@
+/*
+ * 예제22-1
+ * 디지털 충격센서를 이용해서 충격이 감지되면 시리얼 통신으로
+ * 알려주는 프로그램을 만들어보라!
+ * 충격센서는 디지털 2번핀에 연결해라!
+ */
+int sensor = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor = digitalRead(2);
+  //Serial.println(sensor);
+  if(sensor == HIGH){
+    //센서가 작동한것
+    Serial.println("충격이 감지되었습니다!");
+  }
+  delay(100);
+}
