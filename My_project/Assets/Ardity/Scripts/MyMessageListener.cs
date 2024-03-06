d5685dc14a377bbbb5b52b1bf4cfaf1c2d8e9d0e
bakhweebug <parksy8103@gmail.com>
2024-03-06 22:37:19 +0900

    Fix: 메세지 리스너 수정 -> 일단 time이 바뀌긴 함

--- My_project/Assets/Ardity/Scripts/MyMessageListener.cs
+++ My_project/Assets/Ardity/Scripts/MyMessageListener.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
 public class MyMessageListener : MonoBehaviour
 {
+    public AlembicStreamPlayer streamPlayer;
     // Use this for initialization
     void Start()
     {
@@ -11,9 +13,33 @@ public class MyMessageListener : MonoBehaviour
     void Update()
     {
     }
+
+    IEnumerator DelayedLog(float time)
+    {
+        yield return new WaitForSeconds(time); // 주어진 시간만큼 대기
+
+        Debug.Log("Delayed Log at time: " + time);
+    }
     // Invoked when a line of data is received from the serial device.
     void OnMessageArrived(string msg)
     {
+        // 메시지를 받으면 무조건 StartTime을 0부터 3으로 설정
+        if (streamPlayer != null)
+        {
+            streamPlayer.StartTime = 0.0f;
+            streamPlayer.EndTime = 3.0f;
+            streamPlayer.CurrentTime = 0.0f;
+
+            // UpdateImmediately를 호출하여 즉시 업데이트
+            streamPlayer.UpdateImmediately(0.0f);
+            StartCoroutine(DelayedLog(1.0f)); // 1초 후에 DelayedLog 실행
+            streamPlayer.UpdateImmediately(1.0f);
+            StartCoroutine(DelayedLog(1.0f)); // 2초 후에 DelayedLog 실행
+            streamPlayer.UpdateImmediately(2.0f);
+            StartCoroutine(DelayedLog(1.0f)); // 3초 후에 DelayedLog 실행
+
+        }
+
         Debug.Log("Arrived: " + msg);
     }
     // Invoked when a connect/disconnect event occurs. The parameter 'success'
