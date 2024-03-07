95ed5f41f7a59ad329ed8be57535c2bf02eafd93
bakhweebug <parksy8103@gmail.com>
2024-03-07 20:07:26 +0900

    Fix: 진동감지센서 코드 수정

--- vibrationSensor.ino/vibrationSensor.ino.ino
+++ vibrationSensor.ino/vibrationSensor.ino.ino
@@ -1,23 +1,28 @@
-/*
- * 예제22-1
- * 디지털 충격센서를 이용해서 충격이 감지되면 시리얼 통신으로
- * 알려주는 프로그램을 만들어보라!
- * 충격센서는 디지털 2번핀에 연결해라!
- */
 int sensor = 0;
+bool isShockDetected = false; // 충격 감지 여부를 저장하는 변수
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2,INPUT);
+  pinMode(2, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensor = digitalRead(2);
-  //Serial.println(sensor);
-  if(sensor == HIGH){
-    //센서가 작동한것
+  
+  // 센서가 HIGH 값을 감지하고 충격을 감지한 적이 없을 때
+  if (sensor == HIGH && !isShockDetected) {
+    // 충격을 감지했음을 표시
+    isShockDetected = true;
+    // 충격을 감지한 경우에만 메시지를 출력
     Serial.println("0.0f");
+    // 3초 동안 대기
+    delay(100);
   }
-  delay(100);
+  // 센서가 LOW 값을 감지하고 충격을 감지한 적이 있을 때
+  else if (sensor == LOW && isShockDetected) {
+    // 충격을 감지한 적이 없음을 표시
+    isShockDetected = false;
+  }
 }
