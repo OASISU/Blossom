bd119cbc524228e0d2af84e238e272a3535bbce8
bakhwee-bug <parksy8103@gmail.com>
2024-04-09 14:47:01 +0900

    feat: 심박수 받는 동안 모터 왔다갔다 2번

--- pulsetomotor/pulsetomotor.ino
+++ pulsetomotor/pulsetomotor.ino
@@ -3,7 +3,8 @@
 Servo myServo;  // 서보 모터 제어를 위한 서보 객체 생성
 int pulsePin = A0;  // 심박 센서의 데이터를 읽을 아날로그 핀 설정
 int servoPin = 9;   // 서보 모터 연결 핀 설정
-int pulseValue;     // 심박 센서로부터 읽은 값을 저장할 변수
+int pulseValue; 
+int angle = 0;    // 심박 센서로부터 읽은 값을 저장할 변수
 
 void setup() {
   myServo.attach(servoPin);  // 서보 모터 핀을 서보 객체에 연결
@@ -15,9 +16,21 @@ void loop() {
   Serial.println(pulseValue);         // 시리얼 모니터에 심박 데이터 출력
   
   if (pulseValue < 10) {
-    myServo.write(180);  // 심박 데이터가 10 이하일 때, 서보 모터를 최대 속도로 회전시킴
-    delay(5000);         // 5초 동안 계속 회전
-    myServo.write(90);   // 5초 후에 서보 모터를 정지시킴
+    for(angle=0; angle<180; angle++){
+      myServo.write(angle);
+      delay(10);
+    }
+    for(angle=180; angle>0; angle--){
+      myServo.write(angle);
+      delay(10);
+    } for(angle=0; angle<180; angle++){
+      myServo.write(angle);
+      delay(10);
+    }
+    for(angle=180; angle>0; angle--){
+      myServo.write(angle);
+      delay(10);
+    }
   } else {
     // 심박 데이터가 10 이상일 경우 필요한 로직을 추가
   }
